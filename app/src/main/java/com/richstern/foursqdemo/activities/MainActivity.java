fe0b30c16d9c2ffc720d88cf127a46b5314251ed
aguynamedrich <aguynamedrich@gmail.com>
2016-09-16 16:36:52 -0700

    Add libraries and initial map setup

--- app/src/main/java/com/richstern/foursqdemo/activities/MainActivity.java
+++ app/src/main/java/com/richstern/foursqdemo/activities/MainActivity.java
@@ -1,15 +1,116 @@
 package com.richstern.foursqdemo.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.richstern.foursqdemo.R;
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends RxAppCompatActivity implements
+    OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleMap map;
+    private GoogleApiClient googleApiClient;
+
+    private static final int PERM_REQUEST_CODE_LOCATION = 0;
+    private static final String[] LOCATION_PERMISSIONS = new String[] {
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get a reference to the map fragment
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+            .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        // Init api client for location request
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        requestLocation();
+    }
+
+    private void requestLocation() {
+        // Request permissions if necessary
+        if (ActivityCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, LOCATION_PERMISSIONS, PERM_REQUEST_CODE_LOCATION);
+            return;
+        }
+
+        // Request location and init map
+        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        LatLng position = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(position, 11);
+        map.moveCamera(cameraUpdate);
+        map.addMarker(new MarkerOptions().position(position));
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERM_REQUEST_CODE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                requestLocation();
+            }
+        }
     }
 }
